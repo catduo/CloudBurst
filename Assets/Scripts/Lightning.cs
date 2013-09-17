3a8aa89da39457b384c924253e3f4aec819ef437
David Geisert <geisert@stanfordalumni.org>
2013-09-16 18:08:39 -0700

    finished project, Unity crashed so may need to revert

---
+++ Assets/Scripts/Lightning.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lightning : MonoBehaviour {
+	
+	private MeshRenderer meshRenderer;
+	private double flashTime;
+	private double flashDuration = 0.1F;
+	
+	// Use this for initialization
+	void Start () {
+		meshRenderer = GetComponent<MeshRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Time.time > flashTime + flashDuration) {
+			meshRenderer.enabled = false;
+		}
+	}
+	
+	void Flash () {
+		meshRenderer.enabled = true;
+		flashTime = Time.time;
+	}
+}
